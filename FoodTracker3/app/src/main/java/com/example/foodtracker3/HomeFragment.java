76bb96bc087f43b65ab40525a90d39775eef081e
Rick Patneaude <Rick.Patneaude@gmail.com>
2020-05-07 13:45:44 -0700

    remove function test

--- FoodTracker3/app/src/main/java/com/example/foodtracker3/HomeFragment.java
+++ FoodTracker3/app/src/main/java/com/example/foodtracker3/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,19 +22,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         RecyclerView recyclerView;
-        RecyclerView.Adapter adapter;
         RecyclerView.LayoutManager layoutManager;
 
         DatabaseHelper dbh = new DatabaseHelper(this.getActivity() );// Possible memory leak? Store static 'context' in Application class?
-        ArrayList<Product> productList = dbh.getAllProducts();
+        final ArrayList<Product> productList = dbh.getAllProducts();
+
+        final RecyclerView.Adapter adapter;
+        adapter = new Adapter(productList);
+        Button buttonRemove = view.findViewById(R.id.button_remove);
+        final EditText editTextRemove = view.findViewById(R.id.edittext_remove);
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View v) {
+                        int position = Integer.parseInt(editTextRemove.getText().toString());
+                        removeItem(position, productList, adapter);
+            }
+        });
 
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
 
         layoutManager = new LinearLayoutManager(getActivity() );
-        adapter = new Adapter(productList);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
         return view;
+    }
+    public void removeItem(int position, ArrayList<Product> list, RecyclerView.Adapter adapter) {
+        list.remove(position);
+        adapter.notifyDataSetChanged();
     }
 }
