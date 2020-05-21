5c6e8597b72d36be42225668212c7d723d933dd5
villafanam <57918203+villafanam@users.noreply.github.com>
2020-05-21 14:49:25 -0700

    add color and tap notification
    added color
    tap function to notification

--- FoodTracker3/app/src/main/java/com/example/foodtracker3/Adapter.java
+++ FoodTracker3/app/src/main/java/com/example/foodtracker3/Adapter.java
@@ -1,5 +1,9 @@
 package com.example.foodtracker3;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +15,22 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements Filterable {
+import static com.example.foodtracker3.App.expDate_Warning;
+import static com.example.foodtracker3.App.expFood;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements Filterable
+{
+    //-----------------------------------------------------------------------------------
+    private static NotificationManagerCompat notificationManager;
+    //-----------------------------------------------------------------------------------
     public ArrayList<Product> list;
     public ArrayList<Product> listFull;
     private OnItemClickListener listener;
@@ -40,6 +53,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements
         public TextView foodQuantityView;
         public TextView expirationView;
         public ConstraintLayout expandableLayout;
+
         public ViewHolder(@NonNull View itemView, final OnItemClickListener listener)
         {
             super(itemView);
@@ -49,6 +63,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements
             expirationView = itemView.findViewById(R.id.expirationText);
             expiredImageView = itemView.findViewById(R.id.expiredImageView);
             expandableLayout = itemView.findViewById(R.id.expandableLayout);
+
+            //-----------------------------------------------------------------------------------
+            notificationManager = NotificationManagerCompat.from(itemView.getContext());
+            //-----------------------------------------------------------------------------------
 
             itemView.setOnClickListener(new View.OnClickListener() {
                @Override
@@ -75,10 +93,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements
     }
     public Adapter(ArrayList<Product> list)
     {
-
         this.list = list;
         listFull = new ArrayList<>(list);
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
@@ -102,10 +120,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements
         Date today = new Date();
         if(today.after(currentProduct.getExpiration_date())) {     // if expires today, or already expired
             holder.expiredImageView.setImageResource(R.drawable.ic_error_outline);
+            //------------------------------------------------------------------------------
+            sendOnChannel1(holder.itemView, currentProduct);
+            //------------------------------------------------------------------------------
         } else {
 
             if(currentProduct.getExpiration_date().getTime() - today.getTime() <= 345600000) {        // 4 days
                 holder.expiredImageView.setImageResource(R.drawable.ic_warning);
+                //------------------------------------------------------------------------------
+                sendOnChannel2(holder.itemView, currentProduct);
+                //------------------------------------------------------------------------------
             } else {
                 holder.expiredImageView.setImageResource(0);
             }
@@ -153,4 +177,50 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements
             notifyDataSetChanged();
         }
     };
-}
+
+    //-----------------------------------------------------------------------------------
+
+    public static void sendOnChannel1(View v, Product currProduct)
+    {
+        String title = currProduct.getName() +  " EXPIRED!";
+        String message = currProduct.getName() + " has expired as of " + Product.date_toAppStr(currProduct.getExpiration_date()) + "!";
+
+        //defines action when clicked on
+        Intent activityIntent = new Intent(v.getContext(), MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(v.getContext(), 0, activityIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(v.getContext(), expFood)
+                .setSmallIcon(R.drawable.ic_error_outline)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.RED)
+                .setAutoCancel(true) //dismiss notification after tap
+                .setContentIntent(contentIntent)
+                .build();
+        notificationManager.notify(1, notification);
+    }
+
+    public static void sendOnChannel2(View v, Product currProduct)
+    {
+        //defines action when clicked on
+        Intent activityIntent = new Intent(v.getContext(), MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(v.getContext(), 0, activityIntent, 0);
+
+        String title = currProduct.getName() +  " EXPIRED!";
+        String message = currProduct.getName() + " has expired as of " + Product.date_toAppStr(currProduct.getExpiration_date()) + "!";
+
+        Notification notification = new NotificationCompat.Builder(v.getContext(), expDate_Warning)
+                .setSmallIcon(R.drawable.ic_warning)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setColor(Color.YELLOW)
+                .setAutoCancel(true) //dismiss notification after tap
+                .setContentIntent(contentIntent)
+                .build();
+        notificationManager.notify(2, notification);
+    }
+    //-----------------------------------------------------------------------------------
+}//end Adapter
