ffa9061e77f55051f207506350d45a10879a76d4
villafanam <57918203+villafanam@users.noreply.github.com>
2020-06-02 14:22:22 -0700

    Update SplashScreenActivity.java

--- FoodTracker3/app/src/main/java/com/example/foodtracker3/SplashScreenActivity.java
+++ FoodTracker3/app/src/main/java/com/example/foodtracker3/SplashScreenActivity.java
@@ -8,7 +8,8 @@ import android.view.View;
 
 import gr.net.maroulis.library.EasySplashScreen;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,14 +21,10 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .withTargetActivity(MainActivity.class)
                 .withSplashTimeOut(2000) //3 seconds timeout
                 .withBackgroundColor(Color.parseColor("#46403D"))
-                //.withHeaderText("Header")
-                //.withFooterText("Footer")
                 .withBeforeLogoText("WELCOME")
                 .withAfterLogoText("Food inventorying made easy!")
                 .withLogo(R.mipmap.ic_launcher_round);
 
-        //config.getHeaderTextView().setTextColor(Color.WHITE);
-       // config.getFooterTextView().setTextColor(Color.YELLOW);
         config.getBeforeLogoTextView().setTextColor(Color.WHITE);
         config.getAfterLogoTextView().setTextColor(Color.WHITE);
 
