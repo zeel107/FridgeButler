c55b7587b6818081063b545a7165b2e774318ff7
villafanam <57918203+villafanam@users.noreply.github.com>
2020-06-02 13:35:49 -0700

    Added SplashScreen
    Added Splash Screen with 3 second timeout
    action bar still shows up in on full screen

--- FoodTracker3/app/src/main/java/com/example/foodtracker3/ExpJobService.java
+++ FoodTracker3/app/src/main/java/com/example/foodtracker3/ExpJobService.java
@@ -120,7 +120,7 @@ public class ExpJobService extends JobService
     public void sendOnChannel1(Product currProduct)
     {
         String title = currProduct.getName() +  " EXPIRED!";
-        String message = currProduct.getName() + " has expired as of " + DatabaseHelper.date_toAppStr(currProduct.getExpiration_date()) + "!";
+        String message = "Expired as of " + DatabaseHelper.date_toAppStr(currProduct.getExpiration_date()) + "!";
 
         //defines action when clicked on
         Intent activityIntent = new Intent(context, MainActivity.class);
@@ -156,8 +156,8 @@ public class ExpJobService extends JobService
 
     public void sendOnChannel2(Product currProduct)
     {
-        String title = currProduct.getName() +  " expiring!";
-        String message = currProduct.getName() + " will expire on " + DatabaseHelper.date_toAppStr(currProduct.getExpiration_date()) + "!";
+        String title = currProduct.getName() +  " Expiring!";
+        String message = "Expires on " + DatabaseHelper.date_toAppStr(currProduct.getExpiration_date()) + "!";
 
         //defines action when clicked on
         Intent activityIntent = new Intent(context, MainActivity.class);
